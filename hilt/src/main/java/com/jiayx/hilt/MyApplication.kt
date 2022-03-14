93093d0c8f8c619a981a9bfd58157fe326b96ad7
jiayx <yuxi_jj@163.com>
2022-03-14 15:36:00 +0800

    Hilt 注解的引用

---
+++ hilt/src/main/java/com/jiayx/hilt/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.jiayx.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *Created by yuxi_
+on 2022/3/11
+ */
+@HiltAndroidApp
+class MyApplication : Application() {
+}
