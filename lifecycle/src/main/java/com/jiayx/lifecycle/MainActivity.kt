6c5b88d871fa83b2404cae62279bd836b29fc7bc
jiayuxi <yuxi_jj@163.com>
2023-02-21 19:57:17 +0800

    异步加载布局

--- lifecycle/src/main/java/com/jiayx/lifecycle/MainActivity.kt
+++ lifecycle/src/main/java/com/jiayx/lifecycle/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.os.Debug
 import android.os.Looper
 import android.widget.Chronometer
+import androidx.asynclayoutinflater.view.AsyncLayoutInflater
 import androidx.lifecycle.*
 import com.jiayx.lifecycle.databinding.ActivityMainBinding
 import com.jiayx.lifecycle.service.MyService
@@ -27,17 +28,22 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_JetpackStudy)
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
+        //异步加载布局
+        AsyncLayoutInflater(this).inflate(
+            R.layout.activity_main,
+            null
+        ) { view, _, _ ->
+            setContentView(view)
+        }
         lifecycle.addObserver(binding.myChronoMeter)
         lifecycle.addObserver(MyObserver(lifecycle))
         lifecycle.addObserver(viewModel)
         initAction()
-        // idHandler 空闲加载
-        Looper.myQueue().addIdleHandler {
-            Thread.sleep(3000)
-            false
-        }
+        // idHandler 空闲加载 数据
+//        Looper.myQueue().addIdleHandler {
+//            Thread.sleep(3000)
+//            false
+//        }
     }
 
     private fun initAction() {
